028d4ce1ca99011fa6e4fbd79c4680a89e0d454d
Gregor Berginc <gregor.berginc@xlab.si>
2016-09-09 13:43:23 +0200

    Add powerOn and deploy options
    Signed-off-by: Gregor Berginc <gregor.berginc@xlab.si>

--- app/models/manageiq/providers/vmware/cloud_manager/orchestration_service_option_converter.rb
+++ app/models/manageiq/providers/vmware/cloud_manager/orchestration_service_option_converter.rb
@@ -2,7 +2,9 @@ module ManageIQ::Providers
   class Vmware::CloudManager::OrchestrationServiceOptionConverter < ::ServiceOrchestration::OptionConverter
     def stack_create_options
       {
-        :vdc_id => @dialog_options['dialog_availability_zone']
+        :deploy  => stack_parameters['deploy'] == 'true',
+        :powerOn => stack_parameters['powerOn'] == 'true',
+        :vdc_id  => @dialog_options['dialog_availability_zone']
       }
     end
   end
--- app/models/manageiq/providers/vmware/cloud_manager/orchestration_template.rb
+++ app/models/manageiq/providers/vmware/cloud_manager/orchestration_template.rb
@@ -1,9 +1,32 @@
 class ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate < OrchestrationTemplate
   def parameter_groups
     [OrchestrationTemplate::OrchestrationParameterGroup.new(
-      :label      => "Parameters",
-      :parameters => []
+      :label      => "vApp Parameters",
+      :parameters => vapp_parameters,
     )]
+  end
+
+  def vapp_parameters
+    [
+      OrchestrationTemplate::OrchestrationParameter.new(
+        :name          => "deploy",
+        :label         => "Deploy vApp",
+        :data_type     => "boolean",
+        :default_value => false,
+        :constraints   => [
+          OrchestrationTemplate::OrchestrationParameterAllowed.new(:allowed_values => [ "true", "false"])
+        ]
+      ),
+      OrchestrationTemplate::OrchestrationParameter.new(
+        :name          => "powerOn",
+        :label         => "Power On vApp",
+        :data_type     => "boolean",
+        :default_value => false,
+        :constraints   => [
+          OrchestrationTemplate::OrchestrationParameterAllowed.new(:allowed_values => [ "true", "false"])
+        ]
+      )
+    ]
   end
 
   def self.eligible_manager_types
